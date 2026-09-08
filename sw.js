67a52a7e246d9bc0eb1a51486a4bb36dbd8cfc96
rdehullubauduin-dotcom <rdehullubauduin@gmail.com>
2026-09-08 11:05:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // Geen cache, alles gaat direct naar de server zodat de beveiliging intact blijft
+  event.respondWith(fetch(event.request));
+});
